02246ceb814f849625e432098d90e266afd7fd72
DiegoVillacis <dvillacis2229@uta.edu.ec>
2022-01-26 19:16:54 -0500

    Commit 7H015

---
+++ app/src/main/java/app004/flagquizapp/MainInicioVFDI.java
@@ -0,0 +1,18 @@
+package app004.flagquizapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainInicioVFDI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_inicio_vfdi);
+    }
+    
+    
+    
+    
+    
+}
