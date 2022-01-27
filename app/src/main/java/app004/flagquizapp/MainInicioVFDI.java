8df85562e70d437e6c57e7f75c641eb51536e9c3
DiegoVillacis <dvillacis2229@uta.edu.ec>
2022-01-26 20:00:19 -0500

    Commit 8H00

--- app/src/main/java/app004/flagquizapp/MainInicioVFDI.java
+++ app/src/main/java/app004/flagquizapp/MainInicioVFDI.java
@@ -1,18 +1,59 @@
 package app004.flagquizapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainInicioVFDI extends AppCompatActivity {
+
+
+    EditText textUsuarioVFDI, textPassVFDI;
+    Button buttonIngresarVFDI, b2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_inicio_vfdi);
+
+
+        textUsuarioVFDI = (EditText) findViewById(R.id.textUsuarioVFDI);
+        textPassVFDI = (EditText) findViewById(R.id.editTextPassVFDI);
+        buttonIngresarVFDI = (Button) findViewById(R.id.buttonIngresarVFDI);
+
+        buttonIngresarVFDI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = textUsuarioVFDI.getText().toString();
+                String password = textPassVFDI.getText().toString();
+
+
+                if (email == "Diego" && password == "Diego") {
+                    Toast.makeText(getApplicationContext(), "Ha iniciado sesión correctamente", Toast.LENGTH_SHORT).show();
+                    crearInicio();
+                } else
+                    Toast.makeText(getApplicationContext(), "Email o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
-    
-    
-    
-    
-    
+
+    public void crearInicio(){
+        Intent intent = new Intent(this, MainInicioVFDI.class);
+        startActivity(intent);
+    }
+
+    public void onClick(View v) {
+
+        String email = textUsuarioVFDI.getText().toString();
+        String password = textPassVFDI.getText().toString();
+
+        if (email == "Diego" && password == "Diego")
+            crearInicio(); Toast.makeText(getApplicationContext(), "Ha iniciado sesión correctamente", Toast.LENGTH_SHORT).show();
+    }
+
 }
